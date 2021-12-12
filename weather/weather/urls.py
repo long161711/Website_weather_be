47307660a2f6b114d7f3d985a1c84cadbf463b02
long161711 <long161711@gmail.com>
2021-12-12 20:09:42 +0700

    laaaa

--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("forecast.api.urls")),
     path('', include("forecast.urls")),
-    #aloaloalo
+    #may lam gi day
 ]
